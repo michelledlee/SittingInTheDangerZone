91dc3ef09dd62d190adb19a834a31d2c718fbf05
michelledlee <39447664+michelledlee@users.noreply.github.com>
2018-07-13 00:31:59 -0700

    Hooked up buttons and spinners in Java file for settings

--- app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/SettingsActivity.java
+++ app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/SettingsActivity.java
@@ -1,29 +1,121 @@
 package edu.neu.madcourse.michellelee.dangerzone;
 
+import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @Override
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-    }
+        // Initialize Shared Preferences
+        preferences = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+        editor = preferences.edit();
 
+        // Setting up switch to turn notifications on/off
+        final Switch notificationsSwitch = (Switch) findViewById(R.id.notifications_switch);
+        notificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (notificationsSwitch.isChecked()) {
+                    editor.putString("Notifications","On");
+                    Toast.makeText(SettingsActivity.this, "Notifications on", Toast.LENGTH_LONG).show();
+                    editor.apply();
+                } else {
+                    editor.putString("Notifications","Off");
+                    Toast.makeText(SettingsActivity.this, "Notifications off", Toast.LENGTH_LONG).show();
+                    editor.apply();
+                }
+            }
+        });
+
+        // Setting up hours spinner
+        Spinner hoursSpinner = (Spinner) findViewById(R.id.hours_spinner);
+        hoursSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                editor.putInt("Hours", i);
+                editor.apply();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        // Setting up minutes spinner
+        Spinner minutesSpinner = (Spinner) findViewById(R.id.minutes_spinner);
+        minutesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                editor.putInt("Minutes", i);
+                editor.apply();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        // Setting up time pickers
+        Button beforeTime = (Button) findViewById(R.id.select_time1);
+        beforeTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar myCalender = Calendar.getInstance();
+                int hour = myCalender.get(Calendar.HOUR_OF_DAY);
+                int minute = myCalender.get(Calendar.MINUTE);
+
+
+                TimePickerDialog.OnTimeSetListener myTimeListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        if (view.isShown()) {
+                            myCalender.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                            myCalender.set(Calendar.MINUTE, minute);
+
+                        }
+                    }
+                };
+                TimePickerDialog timePickerDialog = new TimePickerDialog(SettingsActivity.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar, myTimeListener, hour, minute, true);
+                timePickerDialog.setTitle("Choose hour:");
+                timePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                timePickerDialog.show();
+            }
+        });
+
+        // Setting up time pickers
+        Button afterTime = (Button) findViewById(R.id.select_time2);
+        afterTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
