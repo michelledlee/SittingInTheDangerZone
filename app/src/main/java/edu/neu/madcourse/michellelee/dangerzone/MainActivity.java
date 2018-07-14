bf67a8dabfcdc39841173ea9b4d97d23b4c717e2
michelledlee <39447664+michelledlee@users.noreply.github.com>
2018-07-14 01:11:32 -0700

    Hooked up dialog to MainActivity on start up

--- app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/MainActivity.java
@@ -1,16 +1,25 @@
 package edu.neu.madcourse.michellelee.dangerzone;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
+
+    private AlertDialog startDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +59,40 @@ public class MainActivity extends AppCompatActivity {
         // If this is the first time we are opening the app
         if (preferences.getBoolean("initial startup", true)) {
             // Dialog to ask for user name
+            AlertDialog.Builder startBuilder = new AlertDialog.Builder(this);
+            LayoutInflater startInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View dialogView = startInflater.inflate(R.layout.initial_start_dialog, null);     // Get dialog view
+            startBuilder.setCancelable(false);
+            startBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    startDialog.dismiss();
+                } });
+            startBuilder.setView(dialogView);    // Set view to initial start dialog
 
+            // Set up username input
+            final EditText usernameEntry = (EditText) dialogView.findViewById(R.id.username_input);
+            // Button to submit username
+            final Button enterUserName = (Button) dialogView.findViewById(R.id.enter_username);
+            enterUserName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                // Username information
+                StringBuilder userName = new StringBuilder();
+                userName.append(usernameEntry.getText());
+                String userNameString = userName.toString();
+                // Text view that shows what the user has submitted their name as
+                TextView welcomeUser = (TextView) dialogView.findViewById(R.id.welcome_message);
+                welcomeUser.setText("Welcome " + userNameString + ".");
+                welcomeUser.setVisibility(View.VISIBLE);
+                enterUserName.setVisibility(View.INVISIBLE);
+                }
+            });
+
+            startDialog = startBuilder.show();
 
             // Update that app has been started up
             editor.putBoolean("initial startup", false);
+            editor.apply();
         }
     }
 
