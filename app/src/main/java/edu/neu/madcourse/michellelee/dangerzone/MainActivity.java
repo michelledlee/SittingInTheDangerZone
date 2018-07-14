d9ed9002b796929519681680bd842b70b48834c5
michelledlee <39447664+michelledlee@users.noreply.github.com>
2018-07-13 23:30:18 -0700

    Added dialog to get information to populate User Profile

--- app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/MainActivity.java
@@ -1,21 +1,31 @@
 package edu.neu.madcourse.michellelee.dangerzone;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Shared Preferences
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = preferences.edit();
+
+        // Ask if this is the first startup of the app as we need to get the user name
+        isInitialStartup();
 
         // Settings button
-        View settingsButton = findViewById(R.id.settings_button);  // create button view
+        View settingsButton = findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) throws RuntimeException {
@@ -24,6 +34,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // User profile
+        View userProfile = findViewById(R.id.profile_button);
+        userProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) throws RuntimeException {
+                Intent userProfileIntent = new Intent(getApplicationContext(), UserProfileActivity.class);
+                startActivity(userProfileIntent);
+            }
+        });
+
+    }
+
+    private void isInitialStartup() {
+        // If this is the first time we are opening the app
+        if (preferences.getBoolean("initial startup", true)) {
+            // Dialog to ask for user name
+
+
+            // Update that app has been started up
+            editor.putBoolean("initial startup", false);
+        }
     }
 
 }
--- app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/UserProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/UserProfileActivity.java
@@ -1,13 +1,44 @@
 package edu.neu.madcourse.michellelee.dangerzone;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    private String name;
+    private int level;
+    private String currentTitle;
+    private int minutesWalked;
+    private int distanceWalked;
+    private int achievements;
+
+    private TextView usernameView;
+    private TextView userLevelView;
+    private TextView userTitleView;
+    private TextView minutesWalkedView;
+    private TextView distancewalkedView;
+    private TextView numberOfAchievementsView;
+
+    private ListView titlesList;
+    private ListView achievementsList;
+
+    private ArrayAdapter<String> titlesAdapter;
+    private ArrayAdapter<String> achievementsAdapter;
+    private ArrayList<String> itemList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +47,38 @@ public class UserProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        // Get user information
+        name = preferences.getString("username", null);
+        level = preferences.getInt("level", -1);
+        currentTitle = preferences.getString("title", null);
+        minutesWalked = preferences.getInt("minutes walked", -1);
+        distanceWalked = preferences.getInt("distance walked", -1);
+        achievements = preferences.getInt("# achievements", -1);
+
+        // Hooking up text views and settings text from loaded user information
+        usernameView = (TextView) findViewById(R.id.user_name);
+        usernameView.setText(name);
+        userLevelView = (TextView) findViewById(R.id.user_level);
+        userLevelView.setText(level);
+        userTitleView = (TextView) findViewById(R.id.user_title);
+        userTitleView.setText(currentTitle);
+        minutesWalkedView = (TextView) findViewById(R.id.minutes_walked);
+        minutesWalkedView.setText(minutesWalked);
+        distancewalkedView = (TextView) findViewById(R.id.distance_walked);
+        distancewalkedView.setText(distanceWalked);
+        numberOfAchievementsView = (TextView) findViewById(R.id.achievements_earned);
+        numberOfAchievementsView.setText(achievements);
+
+        // Hooking up achievements with adapters
+        itemList = new ArrayList<String>();
+        titlesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+        titlesList = (ListView) findViewById(R.id.titles_list);
+        titlesList.setAdapter(titlesAdapter);
+        titlesAdapter.add("title1");
+        achievementsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+        achievementsList = (ListView) findViewById(R.id.achievements_list);
+        achievementsList.setAdapter(achievementsAdapter);
+        achievementsAdapter.add("title1");
     }
 
 }
