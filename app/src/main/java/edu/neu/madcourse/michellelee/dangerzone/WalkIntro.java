8ee689f513c4dd440d0ff83589ab04992e1b9de6
michelledlee <39447664+michelledlee@users.noreply.github.com>
2018-08-09 17:36:33 -0700

    Cleaned up unneeded code

--- app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/WalkIntro.java
+++ app/src/main/java/edu/neu/madcourse/michellelee/dangerzone/WalkIntro.java
@@ -43,43 +43,6 @@ public class WalkIntro extends AppCompatActivity {
             sHandler.postDelayed(sLaunchTask,3000);
             }
         });
-
-//        walkButton = (Button) findViewById(R.id.walk);
-//
-//        // Used to delay the start time once hte walk activity is initiated so the dinosaur can stop roaring first
-//        final Handler mHandler = new Handler();
-//
-//        // Hooking up buttons
-//        final RadioGroup minSelection = (RadioGroup) findViewById(R.id.min_group);
-//        minSelection.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-//            walkButton.setClickable(true);
-//            walkButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                // Dinosaur roars!
-//                mSoundPool.play(mTrexRoar, mVolume, mVolume, 1, 0, 1f);
-//
-//                // Get user selection for walk time
-//                int radioButtonID = minSelection.getCheckedRadioButtonId();
-//                View radioButton = minSelection.findViewById(radioButtonID);
-//                int index = minSelection.indexOfChild(radioButton);
-//
-//                // Get the time selection so we know how long the walk activity should last
-//                if (index == 0) {
-//                    timer = 1;
-//                } else if (index == 1) {
-//                    timer = 3;
-//                } else {
-//                    timer = 5;
-//                }
-//
-//                mHandler.postDelayed(mLaunchTask,3000);
-//                }
-//            });
-//            }
-//        });
     }
 
     /**
